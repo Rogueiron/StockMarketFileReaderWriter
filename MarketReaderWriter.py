1948d535e1d73cc8395826c10e464ffa4f7186f0
Rogueiron <112098171+Rogueiron@users.noreply.github.com>
2023-10-13 14:59:20 -0400

    finished

--- MarketReaderWriter.py
+++ MarketReaderWriter.py
@@ -1,24 +1,20 @@
 try:
-    readorwrite = input("Would you like to (r)ead or (w)override a line or (a)dd")
-
+    readorwrite = input("Would you like to (r)ead or (w)sell or (a)buy: ")
+    percentIncrease = 0.02
     if(readorwrite == "w"):
-
-        fileagain = open("Market.txt","r")
-        overridename = input("Who would you like to override:")
-        linename = fileagain.readline().rstrip('\n')
-        while (overridename != linename):
-            if(linename == " "):
-                print("Couldn't find name")
-            if (overridename == linename.split()[0]):
-                file = open("Market.txt", "w")
-                name = input("Name:")
-                itemNumber = int(input("How much did they buy?:"))
-                price = int(input("what is price per item?:"))
-                linename.replace(linename, name + " " + str(itemNumber) + " " + str(price) + ('\n'))
-                file.close()
-                print("Prices rose by: " + str((price * itemNumber) * 0.2))
-            linename = fileagain.readline().rstrip('\n')
-        fileagain.close()
+        file = open("Market.txt", "r")
+        lines = file.readlines()
+        print(lines)
+        file.close()
+        file = open("Market.txt", "w")
+        lineToChange = int(input("Which line would you like to change: "))
+        name = input("Name:")
+        itemNumber = int(input("How much did they buy?:"))
+        price = int(input("what is price per item?:"))
+        lines[lineToChange - 1] = (name + " " + str(itemNumber) + " " + str(price))
+        file.writelines(lines)
+        file.close()
+        print("Prices rose by: " + str((price * itemNumber) * percentIncrease))
     if(readorwrite == "r"):
         file = open("Market.txt", "r")
         name = file.readline().rstrip('\n')
@@ -27,13 +23,15 @@ try:
             name = file.readline().rstrip('\n')
         file.close()
     if(readorwrite == "a"):
-        file = open("Market.txt", "a")
-        name = input("Name:")
-        itemNumber = int(input("How much did they buy?:"))
-        price = int(input("what is price per item?:"))
-        file.write(name + " " + str(itemNumber) + " " + str(price) + ('\n'))
-        file.close()
-        print("Prices rose by: " + str((price * itemNumber) * 0.2))
+        file = open("Market.txt", "a+")
+        empty = file.readline().rstrip('\n')
+        if(empty == ""):
+            name = input("Name:")
+            itemNumber = int(input("How much did they buy?:"))
+            price = int(input("what is price per item?:"))
+            file.write(name + " " + str(itemNumber) + " " + str(price) + ('\n'))
+            file.close()
+            print("Prices rose by: " + str((price * itemNumber) * percentIncrease))
 
 except FileNotFoundError:
     print("Error")
